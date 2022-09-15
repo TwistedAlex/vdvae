8939e2334e0401e75f7f37bbae9bcc94312cf787
TwistedAlex <muzihuole@gmail.com>
2022-09-14 20:16:31 -0500

    init generating script

---
+++ generate.py
@@ -0,0 +1,22 @@
+import numpy as np
+import imageio
+import os
+import time
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+from data import set_up_data
+from utils import get_cpu_stats_over_ranks
+from train_helpers import set_up_hyperparams, load_vaes, load_opt, accumulate_stats, save_model, update_ema
+
+
+def main():
+    H, logprint = set_up_hyperparams()
+    H, data_train, data_valid_or_test, preprocess_fn = set_up_data(H)
+    vae, ema_vae = load_vaes(H, logprint)
+
+    
+
+
+if __name__ == "__main__":
+    main()
